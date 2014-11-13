f2c0439a564a789c771d5d84d0be084d8ec9f58c
Ewakhine <wenell@hotmail.fr>
2014-11-13 23:43:59 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="robots" content="">
+		<link rel="stylesheet" href="style.css" />
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--[if lte IE 7]>
+		<link rel="stylesheet" href="style_ie.css" />
+		<![endif]-->
+		<title>Union Obscure - Analyses de monstres</title>
+	</head>	
+	
+	<body>
+		<div id="bloc_page">
+			<a id="title" href="index.php"><div id="titre_principal">
+				<h1>Union Obscure</h1>
+				<h2>Analyse de monstres</h2>
+			</div></a>
+			
+			<div id="inter">
